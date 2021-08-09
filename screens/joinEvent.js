488fec7c502cc4a71cc16abe7ccf0452bd97e3e3
Deep <dipesh.khadka9999@gmail.com>
2021-08-08 19:58:09 -0500

    Creat an event and Join event

--- screens/joinEvent.js
+++ screens/joinEvent.js
@@ -1,63 +1,134 @@
 import React, { Component } from "react";
-import { View,StyleSheet, SafeAreaView, TouchableOpacity,Text,Button } from "react-native";
+import { View, StyleSheet, SafeAreaView, TouchableOpacity, Text, Button } from "react-native";
 import fire from "../database/firebase";
 
 export default class JoinEvent extends Component {
-    
-    handlesignout = ()=>{
-        fire.auth().signOut();
-            }
-        
-            render() {
-                const { navigation } = this.props;
-                return (
-                    <SafeAreaView style={{ flex: 1 }}>
-                        <View>
-                        <TouchableOpacity style={styles.card}onPress={()=>{navigation.navigate("CurrentEvent");}} >
-                            <Text style={styles.text}>Rush Creek Park Cleaning</Text>
-                            
-                            
-                        </TouchableOpacity>
-                        </View>
-                    </SafeAreaView>
-                )
-        
-            }
+
+    state = {
+        data: null,
+        loading: true,
+    }
+
+
+    getCurrentEvent = () => {
+
+        var eventID;
+
+
+
+        fire.firestore()
+            .collection("ADMIN")
+            .doc("VFHwReyBcYPWFgEiDEoZfvi3UEr2")
+            .collection("EVENT MANAGEMENT")
+            .where("eventStatus", "==", "current")
+            .get()
+            .then((sub) => {
+                if (sub.docs.length > 0) {
+
+                    const data = []
+                    sub.forEach((doc) => {
+                        const x = doc.data();
+                        x.id = doc.id.toString();
+                        data.push(x);
+
+                    });
+                    this.setState({
+                        data: data
+                    })
+
+                }
+
+            })
+            .then(() => {
+
+                console.log(this.state.data)
+                this.setState({
+                    loading: false
+                })
+
+
+            })
+            .catch((err) => {
+
+                console.log(err.toString())
+            })
+
+
+    }
+
+    joinEvent = () => {
+
+    }
+
+
+
+    componentDidMount() {
+        this.getCurrentEvent();
+    }
+
+
+
+    render() {
+        const { navigation } = this.props;
+
+
+        return (
+            <SafeAreaView style={{ flex: 1 }}>
+                <View>
+
+
+                    <TouchableOpacity style={styles.card} onPress={() => { navigation.navigate("CurrentEvent"); }} >
+
+                        <Text>Rush Creek Park</Text>
+
+                    </TouchableOpacity>
+
+
+                </View>
+            </SafeAreaView>
+        )
+
+
+
+
+
+    }
 }
+
 const styles = StyleSheet.create({
 
     safeview: {
-      backgroundColor:"#a09fdf",
-      height:"100%",
-      width:"100%"
-  
-    },
-    mainView:{
-  
-      height:"100%",
-      width:"100%",
-  
-      flex:1
-    
-    },
-    card:{
-        margin:"5%",
-        backgroundColor:"#D9ACEA" , 
-         justifyContent:"center",
-        alignItems:"center",
-        borderRadius:20,
-       
-    },
-    text:{
-        padding:"10%",
-        fontSize:20,
-       fontWeight:'bold'
-      
+        backgroundColor: "#a09fdf",
+        height: "100%",
+        width: "100%"
 
     },
-    
-  
-   
-  
-    
-  });
+    mainView: {
+
+        height: "100%",
+        width: "100%",
+
+        flex: 1
+
+    },
+    card: {
+        margin: "5%",
+        backgroundColor: "#D9ACEA",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 20,
+
+    },
+    text: {
+        padding: "10%",
+        fontSize: 20,
+        fontWeight: 'bold'
+
+
+    },
+
+
+
+
+
+});
--- screens/startEvent.js
+++ screens/startEvent.js
@@ -1,77 +1,160 @@
 import React, { Component } from "react";
-import { View, SafeAreaView, TouchableOpacity,Text,Button } from "react-native";
+import { View, SafeAreaView, TouchableOpacity, Text, Button, Alert } from "react-native";
 import fire from "../database/firebase";
 import FloatingTextBox from "../assets/textEntry";
+
+
+
+const today = new Date();
+
+function formatDate(date) {
+
+    var MM = date.getMonth() + 1;
+    var DD = date.getDate();
+    var YYYY = date.getFullYear();
+
+
+    return (MM + "-" + DD + "-" + YYYY)
+}
+
+
 export default class StartEvent extends Component {
 
-    state={
-        eventName:"",
-        eventDate:"",
-        eventAddress:"",
-        volunteers:"",
+    state = {
+
+        eventName: "",
+        eventDate: "09-01-2021",
+        eventAddress: "",
+        volunteers: "",
+        eventTime: "10",
+        eventDescription: "Clean the fucking Park mofos!"
+
 
     }
-    
-   
-        
-            render() {
-                const { navigation } = this.props;
-                return (
-                    <SafeAreaView style={{ flex: 1 }}>
-                        <View style={{alignItems:"center"}}>
-                        <FloatingTextBox
-                            label="Event Name"
-                            autoCapitalize="none"
-                            placeholderTextColor="gray"
-                         
-                            onChangeText={(val) => {
-                              this.setState({ eventName: val });
-                             }}
-                            test={this.state.eventName}
-                        ></FloatingTextBox>
-                         <FloatingTextBox
-                            label="Event Date"
-                            autoCapitalize="none"
-                            placeholderTextColor="gray"
-                         
-                            onChangeText={(val) => {
-                              this.setState({ eventDate: val });
-                             }}
-                            test={this.state.eventDate}
-                        ></FloatingTextBox>
-                         <FloatingTextBox
-                            label="Event Address"
-                            autoCapitalize="none"
-                            placeholderTextColor="gray"
-                         
-                            onChangeText={(val) => {
-                              this.setState({ eventAddress: val });
-                             }}
-                            test={this.state.eventAddress}
-                        ></FloatingTextBox>
-                        <FloatingTextBox
-                            label="No.of Volunteers"
-                            autoCapitalize="none"
-                            placeholderTextColor="gray"
-                         
-                            onChangeText={(val) => {
-                              this.setState({ volunteers: val });
-                             }}
-                            test={this.state.volunteers}
-                        ></FloatingTextBox>
-                        
-                        <TouchableOpacity style={{backgroundColor:"red",marginTop:"20%",height:"10%",width:"80%",alignItems:"center"}}>
-                            <Text style={{padding:10,fontSize:20,fontWeight:"bold"}}>Finish</Text>
-                        </TouchableOpacity>
-                       
-                        </View>
-    
-                        
-                            
-                    
-                        
-                    </SafeAreaView>
-                )
-        
-            }
+
+
+
+    startevent = () => {
+
+        console.log("Got here!")
+
+        fire
+            .firestore()
+            .collection("ADMIN")
+            .doc(fire.auth().currentUser.uid.toString())
+            .collection("EVENT MANAGEMENT")
+            .doc()
+            .set({
+                eventName: this.state.eventName,
+                address: this.state.eventAddress,
+                noOfVolunteers: Number(this.state.volunteers),
+                eventStatus: "current",
+                eventDate: "09-01-2021",
+                eventTime: Number("10"),
+                eventDescription: this.state.eventDescription
+
+
+            })
+            .then(() => {
+                Alert.alert("Event Created!");
+                this.props.navigation.goBack();
+
+
+            })
+
+    }
+
+    checkCurrentEvent = () => {
+
+        if (this.state.eventName == "" || this.state.eventDate == "" || this.state.eventAddress == "" || this.state.volunteers == "") {
+            Alert.alert("Please fill all the fields!")
+        } else {
+
+            fire.firestore().collection("ADMIN").doc(fire.auth().currentUser.uid.toString()).collection("EVENT MANAGEMENT").where("eventStatus", "==", "current")
+                .get()
+                .then((snapshot) => {
+                    if (snapshot.empty) {
+                        return this.startevent();
+
+                    }
+                    else {
+                        Alert.alert("Ongoing Event!")
+                    }
+                })
+                .catch((err) => {
+                    console.log(err.toString())
+                    //Alert.alert(err.toString())
+
+                })
+
+
+        }
+
+    }
+
+
+
+    render() {
+        const { navigation } = this.props;
+        return (
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={{ alignItems: "center" }}>
+                    <FloatingTextBox
+                        label="Event Name"
+                        autoCapitalize="none"
+                        placeholderTextColor="gray"
+
+                        onChangeText={(val) => {
+                            this.setState({ eventName: val });
+                        }}
+                        test={this.state.eventName}
+                    ></FloatingTextBox>
+                    {/* <FloatingTextBox
+                        label="Event Date"
+                        autoCapitalize="none"
+                        placeholderTextColor="gray"
+
+                        onChangeText={(val) => {
+                            this.setState({ eventDate: val });
+                        }}
+                        test={this.state.eventDate}
+                    ></FloatingTextBox> */}
+
+                    <FloatingTextBox
+                        label="Event Address"
+                        autoCapitalize="none"
+                        placeholderTextColor="gray"
+
+                        onChangeText={(val) => {
+                            this.setState({ eventAddress: val });
+                        }}
+                        test={this.state.eventAddress}
+                    ></FloatingTextBox>
+                    <FloatingTextBox
+                        label="No.of Volunteers"
+                        autoCapitalize="none"
+                        placeholderTextColor="gray"
+                        keyboardType="number-pad"
+                        onChangeText={(val) => {
+                            this.setState({ volunteers: val });
+                        }}
+                        test={this.state.volunteers}
+                    ></FloatingTextBox>
+
+                    <TouchableOpacity style={{ backgroundColor: "red", marginTop: "20%", height: "10%", width: "80%", alignItems: "center" }}
+                        onPress={this.checkCurrentEvent}
+                    >
+                        <Text style={{ padding: 10, fontSize: 20, fontWeight: "bold" }}>Finish</Text>
+                    </TouchableOpacity>
+
+                </View>
+
+
+
+
+
+            </SafeAreaView>
+        )
+
+    }
 }
