1ca6607bc90c77f988c710d4c57d8b52b643af88
Anubhav <anubhavgautam51@gmail.com>
2021-08-09 20:03:33 -0500

    Fixed Assign Volunteers FrontEnd

--- screens/assignVolunteer.js
+++ screens/assignVolunteer.js
@@ -1,199 +1,257 @@
 import React, { Component } from "react";
-import { View, SafeAreaView, TouchableOpacity, Text, Button, StyleSheet, FlatList, Switch } from "react-native";
+import {
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Text,
+  Button,
+  StyleSheet,
+  FlatList,
+  Switch,
+  TextInput,
+} from "react-native";
 import fire from "../database/firebase";
 import {
-    Spinner,
-    HStack,
-    useColorModeValue,
-    Center,
-    NativeBaseProvider,
+  Spinner,
+  HStack,
+  useColorModeValue,
+  Center,
+  NativeBaseProvider,
+  Select,
 } from "native-base";
-
+import { backgroundColor } from "styled-system";
 
 export default class AssignVolunteer extends Component {
+  _isMounted = false;
 
-    _isMounted = false;
+  state = {
+    data: null,
+    eventID: "",
+    volunteers: null,
+    refreshing: false,
+    selected: {},
+    pickerText: {},
+    status: true,
+    tempStatus: false,
+  };
+  toggleSwitch() {
+    this.setState({
+      status: !this.state.status,
+    });
+  }
+  onPickerSelect(index, idN) {
+    /*s
+        
+        index has the selected item from the list*/
+    // console.log(index);
+    this.setState({
+      selected: { ...this.state.selected, [id]: { zoneNumber: index, id: iN } },
+    });
 
-    state = {
-        data: null,
-        eventID: "",
-        volunteers: null,
-        refreshing: false,
+    console.log(this.state.selected.idN.id.toString());
+  }
 
+  handleRefresh = () => {
+    this.setState(
+      {
+        refreshing: true,
+      },
+      () => {
+        this.componentDidMount();
+      }
+    );
+  };
 
-    }
+  handleChange = (index) => {
+    this.setState({
+      selected: index,
+    });
+  };
 
-    handleRefresh = () => {
-        this.setState(
-            {
-                refreshing: true,
-            },
-            () => {
-                this.componentDidMount();
-            }
-        );
-    };
+  getCurrentEvent = () => {
+    var eventID;
 
+    fire
+      .firestore()
+      .collection("ADMIN")
+      .doc("VFHwReyBcYPWFgEiDEoZfvi3UEr2")
+      .collection("EVENT MANAGEMENT")
+      .where("eventStatus", "==", "current")
+      .get()
+      .then((sub) => {
+        if (sub.docs.length > 0) {
+          const data = [];
+          sub.forEach((doc) => {
+            eventID = doc.id.toString();
+          });
+          this.setState({
+            eventID: eventID,
+          });
+        }
+      })
+      .then(() => {
+        this.getVolunteersInfo();
+      })
+      .catch((err) => {
+        console.log(err.toString());
+      });
+  };
 
+  async getVolunteersInfo() {
+    var data = [];
 
+    return await fire
+      .firestore()
+      .collection("ADMIN")
+      .doc("VFHwReyBcYPWFgEiDEoZfvi3UEr2")
+      .collection("EVENT MANAGEMENT")
+      .doc(this.state.eventID)
+      .collection("VOLUNTEERS")
+      .get()
+      .then(
+        function (querySnapshot) {
+          var results = [];
 
-    getCurrentEvent = () => {
-
-        var eventID;
-
-        fire.firestore()
-            .collection("ADMIN")
-            .doc("VFHwReyBcYPWFgEiDEoZfvi3UEr2")
-            .collection("EVENT MANAGEMENT")
-            .where("eventStatus", "==", "current")
-            .get()
-            .then((sub) => {
-                if (sub.docs.length > 0) {
-
-                    const data = []
-                    sub.forEach((doc) => {
-
-                        eventID = doc.id.toString();
-
-
-                    });
-                    this.setState({
-
-                        eventID: eventID
-                    })
-
-                }
-
-            })
-            .then(() => {
-
-
-                this.getVolunteersInfo();
-
-
-
-            })
-            .catch((err) => {
-
-                console.log(err.toString())
-            })
-
-
-    }
-
-
-    async getVolunteersInfo() {
-        var data = [];
-
-        return await fire
-            .firestore()
-            .collection("ADMIN")
-            .doc("VFHwReyBcYPWFgEiDEoZfvi3UEr2")
-            .collection("EVENT MANAGEMENT")
-            .doc(this.state.eventID)
-            .collection("VOLUNTEERS")
-            .get()
-            .then(
-                function (querySnapshot) {
-                    var results = [];
-
-                    querySnapshot.forEach(function (doc) {
-                        var docRef = fire
-                            .firestore()
-                            .collection("VOLUNTEER")
-                            .doc(doc.id)
-                            .get()
-                            .then(
-                                function (snap) {
-                                    const x = snap.data();
-                                    x.id = doc.id;
-                                    data.push(x);
-                                },
-                                function (error) {
-                                    Alert.alert(error.toString());
-                                }
-                            );
-                        // push promise from get into results
-                        results.push(docRef);
-                    });
-                    // dbPromise.then() resolves to  a single promise that resolves
-                    // once all results have resolved
-                    return Promise.all(results);
+          querySnapshot.forEach(function (doc) {
+            var docRef = fire
+              .firestore()
+              .collection("VOLUNTEER")
+              .doc(doc.id)
+              .get()
+              .then(
+                function (snap) {
+                  const x = snap.data();
+                  x.id = doc.id;
+                  data.push(x);
                 },
-                function (val) {
-                    // The Promise was rejected.
-                    console.log(val);
+                function (error) {
+                  Alert.alert(error.toString());
                 }
-            )
-            .then(() => {
-                this.setState({
-                    data: data,
-                    refreshing: false
+              );
+            // push promise from get into results
+            results.push(docRef);
+          });
+          // dbPromise.then() resolves to  a single promise that resolves
+          // once all results have resolved
+          return Promise.all(results);
+        },
+        function (val) {
+          // The Promise was rejected.
+          console.log(val);
+        }
+      )
+      .then(() => {
+        this.setState({
+          data: data,
+          refreshing: false,
+        });
+      })
+      .then(() => {
+        console.log(this.state.data);
+      })
+      .catch(function (error) {
+        console.log("Error getting documents: ", error);
+      });
+  }
 
-                });
-            })
-            .then(() => {
-                console.log(this.state.data)
-            })
-            .catch(function (error) {
-                console.log("Error getting documents: ", error);
-            });
-    }
+  emptyComponent = () => {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text style={{ fontSize: 20, fontStyle: "italic", fontWeight: "bold" }}>
+          No Volunteers Signed Up Yet...
+        </Text>
+      </View>
+    );
+  };
 
-    emptyComponent = () => {
-        return (
-            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                <Text style={{ fontSize: 20, fontStyle: "italic", fontWeight: "bold", }}>
-                    No Volunteers Signed Up Yet...
-                </Text>
-            </View>
-        );
-    };
+  componentDidMount() {
+    this._isMounted = true;
+    this.getCurrentEvent();
+  }
 
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
 
+  render() {
+    const { navigation } = this.props;
 
-    componentDidMount() {
-        this._isMounted = true;
-        this.getCurrentEvent();
-    }
-
-    componentWillUnmount() {
-        this._isMounted = false
-    }
-
-
-
-
-    render() {
-        return (
-            <SafeAreaView style={{ flex: 1, }}>
-                {this.state.data ? (
-                    <FlatList
-                        data={this.state.data}
-                        renderItem={({ item }) => (
-                            <View >
-                                <Text>{item.fName}</Text>
-                            </View>
-                        )}
-                        keyExtractor={(item) => item.email}
-                        ListEmptyComponent={this.emptyComponent}
-                        refreshing={this.state.refreshing}
-                        onRefresh={this.handleRefresh}
-                    />
-                ) : (
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        {this.state.data ? (
+          <FlatList
+            data={this.state.data}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate("ReassignVolunteers",{itemId: item.id, fname:item.fName, lname:item.lName, email:item.email});
+                }}
+                style={{
+                  margin: "4%",
+                  backgroundColor: "lightblue",
+                  justifyContent: "space-between",
+                  borderRadius: 10,
+                }}
+              >
+                <View>
+                  <Text>{item.fName}</Text>
+                </View>
+                <View>
+                  <Text>{item.email}</Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <View>
                     <NativeBaseProvider>
-                        <Center flex={1}>
-                            <Spinner color="blue.500" />
-                        </Center>
+                      <Select
+                        placeholder="Zone"
+                        placeholderTextColor="black"
+                        width={150}
+                        onValueChange={(itemValue) =>
+                          this.onPickerSelect(itemValue, item.id)
+                        }
+                      >
+                        <Select.Item label="Zone 1" value={1} />
+                        <Select.Item label="Zone 2" value={2} />
+                        <Select.Item label="Zone 3" value={3} />
+                        <Select.Item label="Zone 4" value={4} />
+                        <Select.Item label="Zone 5" value={5} />
+                        <Select.Item label="Zone 6" value={6} />
+                      </Select>
                     </NativeBaseProvider>
+                  </View>
 
-                )}
-            </SafeAreaView>
-        );
-    }
+                  <View>
+                    <Switch
+                      trackColor={{ false: "#767577", true: "#81b0ff" }}
+                      thumbColor={this.state.status ? "#f5dd4b" : "#f4f3f4"}
+                      ios_backgroundColor="#3e3e3e"
+                      onValueChange={this.toggleSwitch.bind(this)}
+                      value={this.state.status}
+                    />
+                  </View>
+                </View>
+              </TouchableOpacity>
+            )}
+            keyExtractor={(item) => item.email}
+            ListEmptyComponent={this.emptyComponent}
+            refreshing={this.state.refreshing}
+            onRefresh={this.handleRefresh}
+          />
+        ) : (
+          <NativeBaseProvider>
+            <Center flex={1}>
+              <Spinner color="blue.500" />
+            </Center>
+          </NativeBaseProvider>
+        )}
+      </SafeAreaView>
+    );
+  }
 }
 
-
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
--- screens/reassignVolunteer.js
+++ screens/reassignVolunteer.js
@@ -1,26 +1,152 @@
+import { VStack } from "native-base";
 import React, { Component } from "react";
-import { View, SafeAreaView, TouchableOpacity,Text,Button } from "react-native";
+import {
+  View,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+} from "react-native";
 import fire from "../database/firebase";
+import {
+  Spinner,
+  HStack,
+  useColorModeValue,
+  Center,
+  NativeBaseProvider,
+  Select,
+} from "native-base";
 
 export default class ReassignVolunteers extends Component {
-    
-    handlesignout = ()=>{
-        fire.auth().signOut();
-            }
-        
-            render() {
-                const { navigation } = this.props;
-                return (
-                    <SafeAreaView style={{ flex: 1 }}>
-                        <View>
-                            <Text>Admin Home page</Text>
-                            <Button
-                            title="SIGNOUT"
-                            onPress={this.handlesignout}
-                            ></Button>
-                        </View>
-                    </SafeAreaView>
-                )
-        
-            }
+  state = {
+    userId: "",
+    Zone: "",
+    zoneplaceHolder: "Select Zone",
+    leader: false,
+    status: true,
+    statusLabel:"Active"
+  };
+
+  onPressUpdate = (val) => {
+    //handle updating data base shit
+
+    console.log(val);
+  };
+
+  handlesignout = () => {
+    fire.auth().signOut();
+  };
+
+  toggleSwitch() {
+    this.setState({
+      leader: !this.state.leader,
+     
+    });
+  }
+
+  statusSwitch=()=>{
+     this.setState({
+         status: !this.state.status,
+     })
+
+  }
+ 
+
+  onPickerSelect = (value) => {
+    this.setState({
+      Zone: value,
+      zoneplaceHolder: value.toString(),
+    });
+  };
+
+  render() {
+    const { navigation } = this.props;
+    const itemid = this.props.route.params.itemId;
+    const fname = this.props.route.params.fname;
+    const lname = this.props.route.params.lname;
+    const email = this.props.route.params.email;
+
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.mainView}>
+          <TouchableOpacity style={styles.card}>
+            <Text style={styles.headerText}>{fname}</Text>
+            <Text style={styles.headerText}>{lname}</Text>
+            <Text style={styles.text}>{email}</Text>
+            <View style={{ flexDirection: "row", marginTop: "10%" }}>
+              <Text style={styles.headerText}>Leader</Text>
+              <Switch
+                trackColor={{ false: "yellow", true: "green" }}
+                thumbColor={this.state.leader ? "white" : "red"}
+                onValueChange={this.toggleSwitch.bind(this)}
+                value={this.state.leader}
+              />
+            </View>
+            <View style={{ flexDirection: "row", marginTop: "5%" }}>
+              <NativeBaseProvider>
+                <Select
+                  placeholder={this.state.zoneplaceHolder}
+                  placeholderTextColor="black"
+                  width={150}
+                  margin="5%"
+                  onValueChange={(itemValue) => this.onPickerSelect(itemValue)}
+                >
+                  <Select.Item label="Zone 1" value={1} />
+                  <Select.Item label="Zone 2" value={2} />
+                  <Select.Item label="Zone 3" value={3} />
+                  <Select.Item label="Zone 4" value={4} />
+                  <Select.Item label="Zone 5" value={5} />
+                  <Select.Item label="Zone 6" value={6} />
+                </Select>
+              </NativeBaseProvider>
+
+            </View>
+            <View>
+                <Text style={styles.headerText}>Status</Text>
+            <Switch
+                placeholder="Status"
+                trackColor={{ false: "red", true: "green" }}
+                thumbColor={this.state.status ? "white" : "black"}
+                onValueChange={this.statusSwitch.bind(this)}
+                value={this.state.status}
+              />
+            </View>
+           
+          </TouchableOpacity>
+          <Button title="Update" onPress={this.onPressUpdate}></Button>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
+const styles = StyleSheet.create({
+  safeview: {
+    backgroundColor: "#a09fdf",
+    height: "100%",
+    width: "100%",
+  },
+  mainView: {
+    height: "100%",
+    width: "100%",
+
+    flex: 1,
+  },
+  card: {
+    margin: "5%",
+    backgroundColor: "#D9ACEA",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  text: {
+    fontSize: 15,
+    fontWeight: "bold",
+    marginTop: "2%",
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
