212010d42ea25d141768cc917ec3464e3db01b68
Anubhav <anubhavgautam51@gmail.com>
2021-08-11 06:26:25 -0500

    Report Object Completed UI

--- screens/reportObject.js
+++ screens/reportObject.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import * as ImagePicker from "expo-image-picker";
 import {
   View,
+  Image,
   TextInput,
   SafeAreaView,
   TouchableOpacity,
@@ -12,7 +14,7 @@ import {
 import fire from "../database/firebase";
 import { NativeBaseProvider, Select } from "native-base";
 import { Icon } from "react-native-elements";
-import * as Location from 'expo-location';
+import * as Location from "expo-location";
 import FloatingTextBox from "../assets/textEntry";
 
 export default class ReportObject extends Component {
@@ -20,18 +22,114 @@ export default class ReportObject extends Component {
     userId: "",
     Zone: "",
     zoneplaceHolder: "Select Zone",
-   description:"",
-    location:"",
+    description: "",
+    location: "",
+    resultUri: " ",
+    downloadUri:"",
+    uploading:false,
+  };
+
+  componentDidMount = () => {
+     
+    async () => {
+      if (Platform.OS !== "web") {
+        const { status } =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== "granted") {
+          alert("Sorry, we need camera roll permissions to make this work!");
+        }
+      }
+    };
+  };
+  pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+
+    if (!result.cancelled) {
+      this.setState({ resultUri: result.uri });
+      console.log("State")
+      console.log(this.state.resultUri);
+   
+    }
+    
   };
   handlesignout = () => {
     fire.auth().signOut();
   };
-  setLocation=(value)=>{
-      this.setState({
-            location: value
-            
-      });
+  setLocation = (value) => {
+    this.setState({
+      location: value,
+    });
+  };
+  checkEmpty=()=>{
+      if(this.state.Zone!=""&& this.state.description!=""&&this.state.resultUri!=" ")
+      { 
+        this.uploadImage();
+          
+      }
+      else if (this.state.Zone=="")
+      {
+          alert("Please select a Zone")
+          
+      }
+      else if (this.state.description=="")
+      {
+          alert("Please Provide Description")
+          
+      }
+      else{
+          alert("Please upload a picture")
+      }
+
+  };
+
+  handleReport=()=>{
+      
   }
+
+
+  uploadImage =async()=>{
+
+    const blob  = await new Promise((resolve, reject) => {
+          const xhr = new XMLHttpRequest();
+          xhr.onload = function () {
+            resolve(xhr.response);
+          };
+          xhr.onerror = function (e) {
+            console.log(e);
+            reject(new TypeError('Network request failed'));
+         
+          };
+          xhr.responseType = 'blob';
+          xhr.open('GET',this.state.resultUri, true);
+          xhr.send(null);
+        }
+        );
+ const ref= fire.storage().ref().child(new Date().toISOString())
+await ref.put(blob) 
+ const url = await ref.getDownloadURL().catch((error) => { throw error });
+
+this.setState({
+ downloadUri:url
+ 
+});
+this.handleReport();
+
+
+
+ }
+
+
+    
+    
+  
+
+ 
 
   onPickerSelect = (value) => {
     this.setState({
@@ -39,20 +137,21 @@ export default class ReportObject extends Component {
       zoneplaceHolder: value.toString(),
     });
   };
-handleGeoTag =()=>{
+  handleGeoTag = () => {
     //handle geo tagging here
-}
+    alert("Your locaton was saved");
+  };
 
   render() {
     const { navigation } = this.props;
     return (
-      <SafeAreaView>
-        <View>
+      <SafeAreaView style={styles.safeview}>
+     
           <View style={styles.rowView}>
             <NativeBaseProvider>
               <Select
                 placeholder={this.state.zoneplaceHolder}
-                backgroundColor="lightblue"
+                backgroundColor="#ba84d1"
                 placeholderTextColor="black"
                 width={"50%"}
                 onValueChange={(itemValue) => this.onPickerSelect(itemValue)}
@@ -66,37 +165,47 @@ handleGeoTag =()=>{
               </Select>
             </NativeBaseProvider>
           </View>
-         
 
           <TextInput
-        style={styles.input}
-        onChangeText={(val)=>{
-            this.setState({description:val});
-        }}
-        value={this.state.description}
-        placeholder="Description"
-        placeholderTextColor="purple"
-       
-      />
-                   
+            style={styles.input}
+            onChangeText={(val) => {
+              this.setState({ description: val });
+            }}
+            value={this.state.description}
+            placeholder="Description"
+            placeholderTextColor="purple"
+          />
+          <View style={styles.rowView}>
           <TouchableOpacity
             style={styles.card}
-            onPress={() => {
-              alert("Your location was saved");
-            }}
+            onPress={this.handleGeoTag}
           >
             <Text style={styles.text}>Geo Tag</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.card}
-            onPress={() => {
-              alert("Report Sent");
-              navigation.navigate("CurrentEvent");
-            }}
+            onPress={this.pickImage}
           >
-            <Text style={styles.text}>Report </Text>
+            <Text style={styles.text}>Image </Text>
           </TouchableOpacity>
+
+          </View>
+            <View style={{alignItems:'center'}}>
+            <Image source={{uri:this.state.resultUri}} style={{height:150, width:150}}></Image>
+            </View>
+          
+          
+    
+        <View style={{alignItems:"center"}}>
+        <TouchableOpacity
+            style={styles.card}
+            onPress={this.checkEmpty}
+          >
+            <Text style={styles.text}>Report</Text>
+          </TouchableOpacity>
+        
         </View>
+       
       </SafeAreaView>
     );
   }
@@ -115,16 +224,20 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   card: {
-    margin: "5%",
-    backgroundColor: "lightblue",
-    justifyContent: "center",
 
+  margin:"5%",
+    backgroundColor: "#ba84d1",
+    justifyContent:'center',
+    alignItems:"center",
+    
     borderRadius: 20,
   },
   text: {
-    fontSize: 15,
+    fontSize: 20,
     fontWeight: "bold",
     marginTop: "2%",
+    padding:"5%"
+    
   },
   headerText: {
     fontSize: 20,
@@ -136,13 +249,14 @@ const styles = StyleSheet.create({
     marginVertical: "5 %",
     justifyContent: "space-between",
     marginLeft: "2%",
+    
   },
-  input:{
-  height: "20%",
-  margin: "2%",
-  borderWidth: 1,
-  borderRadius:12,
-  borderColor:"blue",
-  padding: "5%"
-  }
+  input: {
+    height: "20%",
+    margin: "2%",
+    borderWidth: 1,
+    borderRadius: 12,
+    borderColor: "blue",
+    padding: "5%",
+  },
 });
