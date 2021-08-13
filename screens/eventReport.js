67909d38873b0d42d7f093912cbecfd6530e43c6
Deep <dipesh.khadka9999@gmail.com>
2021-08-13 01:41:30 -0500

    Changes

--- screens/eventReport.js
+++ screens/eventReport.js
@@ -21,9 +21,6 @@ export default class EventReport extends Component {
     changeStatus = (zone, id, eventID) => {
 
 
-
-
-
         fire
             .firestore()
             .collection("ADMIN")
